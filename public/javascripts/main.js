c9b94ce9ea4a4602b6fb38b458c6f6b0937dec07
Justin Martin <justspamjustin@gmail.com>
2012-09-25 18:50:44 -0600

    Start from scratch

--- public/javascripts/main.js
+++
@@ -1,8 +0,0 @@
-require(['views/HelloWorldView'],function(HelloWorldView){
-    var helloWorldView = new HelloWorldView({
-       el:$("#inner-pages")
-    });
-
-    helloWorldView.render();
-
-});
--- public/javascripts/views/HelloWorldView.js
+++
@@ -1,9 +0,0 @@
-define(['text!templates/hello-world-template.handlebars'],function(helloWorldTemplate){
-    return Backbone.View.extend({
-        render: function() {
-            var template = Handlebars.compile(helloWorldTemplate);
-            this.$el.html(template());
-            return this;
-        }
-    });
-});
