9bf8cd605ad5c64133b414ff21812244d5d9462a
justspamjustin <justspamjustin@gmail.com>
2012-10-25 13:48:35 -0600

    Has working delete button.

--- mobile/app/controller/TasksList.js
+++ mobile/app/controller/TasksList.js
@@ -3,20 +3,48 @@ Ext.define('mobile.controller.TasksList', {
   config: {
     control: {
       '#tasksList': {
-        itemtap: 'onItemTap'
+        itemtap: 'onItemTap',
+        itemswipe: 'onItemSwipe'
       }
     }
   },
   onItemTap: function(container, item, index,target, e, eOpts) {
     var $checkbox = $(index.dom).find('.task-checkbox');
     var $target = $(e.browserEvent.target);
-    if($checkbox.has($target).length > 0) {
-      var modelId = target.data.id;
-      var model = Ext.getStore('Tasks').getById(modelId);
-      model.set('completed',!target.data.completed);
+    var $deleteButton = $target.find('.delete-task');
+    if(!$deleteButton.hasClass('showing')) {
+      if($checkbox.has($target).length > 0 ||
+        $checkbox.is($target)) {
+        this.onTapCheckbox(target.data);
+      } else if($deleteButton.has($target)) {
+        this.onTapDelete(target.data);
+      } else {
+        //todo: go to details
+      }
     } else {
-      //todo: go to details for this item
+      $deleteButton.removeClass('showing');
     }
+  },
+
+  onItemSwipe: function(container, item, index,target, e, eOpts) {
+    var $target = $(e.browserEvent.target);
+    var $deleteButton = $target.find('.delete-task');
+    if(e.direction == 'left'
+      && !$deleteButton.hasClass('showing')) {
+      $deleteButton.addClass('showing');
+    } else if($deleteButton.hasClass('showing')) {
+      $deleteButton.removeClass('showing');
+    }
+  },
+
+  onTapCheckbox: function(data) {
+    var model = Ext.getStore('Tasks').getById(data.id);
+    model.set('completed',!data.completed);
+  },
+
+  onTapDelete: function(data) {
+    var model = Ext.getStore('Tasks').getById(data.id);
+    model.destroy();
   }
 
 
