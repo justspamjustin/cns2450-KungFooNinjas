515db2552e5dc1d5032e638039913aa6f8dd6779
unknown <10067253@uvlink.uvu.edu>
2012-11-27 17:07:04 -0700

    added subtask classes to server

--- app/controllers/SubTask.java
+++ app/controllers/SubTask.java
@@ -2,12 +2,36 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
+//Author Dan
+
 package controllers;
 
-/**
- *
- * @author Dan
- */
-public class SubTask {
-    
+import com.google.gson.*;
+import models.SubTaskModel;
+import play.libs.IO;
+import play.mvc.Controller;
+
+import java.util.Date;
+import java.util.List;
+
+public class SubTask extends Controller {
+public static void createSubTask() throws Exception {
+        String requestString = IO.readContentAsString(request.body);
+        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+        JsonParser jsonParser = new JsonParser();
+        JsonObject json = jsonParser.parse(requestString).getAsJsonObject();
+        json.remove("id");
+        SubTaskModel subTaskModel = gson.fromJson(json,SubTaskModel.class);
+
+        String currentUserIdString = User.getUserId();
+        //subTaskModel.taskId = Long.parseLong(currentUserIdString);
+        subTaskModel.createdDate = new Date();
+        subTaskModel.completed = false;
+        subTaskModel.save();
+
+        JsonObject responseObject = new JsonObject();
+        responseObject.addProperty("MESSAGE", "Success");
+        renderJSON(responseObject.toString());
+    }
 }
--- app/models/SubTaskModel.java
+++ app/models/SubTaskModel.java
@@ -2,12 +2,35 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
+//Author Dan
+
 package models;
 
-/**
- *
- * @author Dan
- */
-public class SubTaskModel {
-    
+import play.db.jpa.Model;
+
+import javax.persistence.Entity;
+import java.util.Date;
+
+@Entity
+public class SubTaskModel extends Model {
+
+    public Long taskId;
+    public Date createdDate;
+
+    public String subTaskName;
+    public String reminderMessage;
+    public Date dueDate;
+    public Date reminderDate;
+    public Boolean completed;
+
+    public SubTaskModel(Long taskId, Date createdDate, Date dueDate, Date reminderDate, String reminderMessage, Boolean completed) {
+        this.taskId = taskId;
+	this.subTaskName = subTaskName;
+        this.createdDate = createdDate;
+        this.dueDate = dueDate;
+        this.reminderDate = reminderDate;
+        this.reminderMessage = reminderMessage;
+        this.completed = completed;
+    }
 }
